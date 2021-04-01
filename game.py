e0ee6c5a186f584f9b27ab1d059d4cc67cc44b65
sara.vikstrom.edvinsson <saed0037@ad.umu.se>
2021-04-01 09:53:41 +0200

    first commit

---
+++ game.py
@@ -0,0 +1,15 @@
+import os
+
+def runtestcode(l):
+    print("-"*20)
+    for e in l:
+        print(e)
+    print("-"*20)
+
+runtestcode([5, 3, 2, 4, 1, 7, 9])
+input()
+os.system('cls' if os.name == 'nt' else 'clear')
+runtestcode([5,6,42,00000lajgfi])
+input()fda
+
+os.system('cls' if os.name == 'nt' else 'clear')
