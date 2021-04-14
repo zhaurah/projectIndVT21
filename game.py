2bd7067c01cb5869771bed5c5d4b3e6f7ca854fc
Zhaurah <zhaurah@gmail.com>
2021-04-14 15:47:29 +0200

    menu implemnte

--- game.py
+++ game.py
@@ -10,6 +10,6 @@ runtestcode([5, 3, 2, 4, 1, 7, 9])
 input()
 os.system('cls' if os.name == 'nt' else 'clear')
 runtestcode([5,6,42,11])
-input()fda
+input()
 
 os.system('cls' if os.name == 'nt' else 'clear')
